4d68f72751282eb817fba04d5cb1ac358cfeb5e2
magaum <weslei_luiz22@hotmail.com>
2018-04-03 12:31:54 -0300

    edicao dos controllers

--- src/main/java/br/com/estatistica/estatistica/controller/ExerciseControllerMedia.java
+++ src/main/java/br/com/estatistica/estatistica/controller/ExerciseControllerMedia.java
@@ -1,5 +1,22 @@
 package br.com.estatistica.estatistica.controller;
+import org.telegram.telegrambots.api.objects.Update;
 
-public class ExercicioControllerMedia {
+import br.com.estatistica.estatistica.model.AppModel;
+import br.com.estatistica.estatistica.view.AppView;
 
+
+public class ExerciseControllerMedia implements ExerciseController{
+
+	private AppModel model;
+	private AppView view;
+	
+	public ExerciseControllerMedia(AppModel model, AppView view) {
+		this.model = model;
+		this.view = view;
+	}
+	
+	public void calcular(Update update) {
+		//model.calculaMedia(update);
+		//view.sendTypingMessage(update);
+	}	
 }
--- src/main/java/br/com/estatistica/estatistica/controller/ExerciseControllerModa.java
+++ src/main/java/br/com/estatistica/estatistica/controller/ExerciseControllerModa.java
@@ -1,5 +1,22 @@
 package br.com.estatistica.estatistica.controller;
 
-public class ExercicioControllerModa {
+import org.telegram.telegrambots.api.objects.Update;
 
+import br.com.estatistica.estatistica.model.AppModel;
+import br.com.estatistica.estatistica.view.AppView;
+
+public class ExerciseControllerModa implements ExerciseController {
+
+	private AppModel model;
+	private AppView view;
+
+	public ExerciseControllerModa(AppModel model, AppView view) {
+		this.model = model;
+		this.view = view;
+	}
+
+	public void calcular(Update update) {
+		// model.calculaMedia(update);
+		// view.sendTypingMessage(update);
+	}
 }
