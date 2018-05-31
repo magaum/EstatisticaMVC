d20d11095756117ce7931a89529f06c512f5383e
magaum <weslei_luiz22@hotmail.com>
2018-05-31 02:37:53 -0300

    correcao de arquivos versionados

--- src/main/java/br/com/estatistica/estatistica/view/View.java
+++ src/main/java/br/com/estatistica/estatistica/view/View.java
@@ -13,9 +13,6 @@ import com.pengrad.telegrambot.request.SendChatAction;
 import com.pengrad.telegrambot.request.SendDocument;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SendPhoto;
-import com.pengrad.telegrambot.response.BaseResponse;
-import com.pengrad.telegrambot.response.SendResponse;
-
 import br.com.estatistica.estatistica.controller.ActionController;
 import br.com.estatistica.estatistica.controller.ExerciseControllerMean;
 import br.com.estatistica.estatistica.controller.ExerciseControllerMedian;
@@ -29,9 +26,6 @@ public class View implements Observer {
 	private Model model;
 	private boolean waitUserInput = true;
 	private TelegramBot bot;
-
-	// Object that send responses
-	private SendResponse sendResponse;
 	// Strategy Pattern -- connection View -> Controller
 	private ActionController actionController;
 
@@ -74,8 +68,7 @@ public class View implements Observer {
 
 			if (message.equalsIgnoreCase("Relatório de requisições")) {
 				setController(new HistoricController(model, this));
-				sendResponse = bot
-						.execute(new SendMessage(chatId, "Estou consultando valores armazenados, já respondo"));
+				bot.execute(new SendMessage(chatId, "Estou consultando valores armazenados, já respondo"));
 				this.waitUserInput = false;
 
 			}
@@ -83,18 +76,18 @@ public class View implements Observer {
 				this.callController(update);
 			} else if (message.equalsIgnoreCase("Media")) {
 				setController(new ExerciseControllerMean(model, this));
-				sendResponse = bot.execute(new SendMessage(chatId,
+				bot.execute(new SendMessage(chatId,
 						"Entendi, media! Então digita os valores de entrada separados por ponto e virgula ; para eu poder calcular que já respondo"));
 				this.waitUserInput = false;
 
 			} else if (message.equalsIgnoreCase("Mediana")) {
 				setController(new ExerciseControllerMedian(model, this));
-				sendResponse = bot.execute(new SendMessage(chatId,
+				bot.execute(new SendMessage(chatId,
 						"Entendi, mediana! Então digita os valores de entrada separados por ponto e virgula ; para eu poder calcular que já respondo"));
 				this.waitUserInput = false;
 			} else if (message.equalsIgnoreCase("Moda")) {
 				setController(new ExerciseControllerMode(model, this));
-				sendResponse = bot.execute(new SendMessage(chatId,
+				bot.execute(new SendMessage(chatId,
 						"Entendi, moda! Então digita os valores de entrada separados por ponto e virgula ; para eu poder calcular que já respondo"));
 				this.waitUserInput = false;
 
@@ -117,25 +110,24 @@ public class View implements Observer {
 	}
 
 	public void sendTypingMessage(Update update) {
-		BaseResponse baseResponse = bot
-				.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+		bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
 	}
 
 	@Override
 	public void update(long chatId, String data) {
-		sendResponse = bot.execute(new SendMessage(chatId, data));
+		bot.execute(new SendMessage(chatId, data));
 		this.waitUserInput = true;
 	}
 
 	@Override
-	public void sendImage(long chatId,File img) {
-		sendResponse = bot.execute(new SendPhoto(chatId, img));
+	public void sendImage(long chatId, File img) {
+		bot.execute(new SendPhoto(chatId, img));
 		this.waitUserInput = true;
 	}
 
 	@Override
 	public void sendDocument(long chatId, File file) {
-		sendResponse = bot.execute(new SendDocument(chatId, file).fileName("historico.pdf"));
+		bot.execute(new SendDocument(chatId, file).fileName("historico.pdf"));
 		this.waitUserInput = true;
 	}
 }
---
+++ src/main/resources/readme.md
@@ -0,0 +1,3 @@
+Substitua o arquivo token example com seu token gerado pelo bot father como no arquivo token.example.
+
+O arquivo deve se chamar token.properties, assim como no arquivo log4j.example deve se chamar log4j.properties.
